64fab22933fc9e6cf31ab1fbf8a13cada013f2e9
Maciej Marczak <maciejmarczak@hotmail.com>
2017-11-29 21:06:54 +0100

    Add directive for displaying form validation messages

--- web-client/src/app/shared/shared.module.ts
+++ web-client/src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ValidationFeedbackDirective } from './validation-feedback.directive';
 
 @NgModule({
   declarations: [
+    ValidationFeedbackDirective
   ],
   exports: [
+    ValidationFeedbackDirective
   ]
 })
 export class SharedModule {}
---
+++ web-client/src/app/shared/validation-feedback.directive.ts
@@ -0,0 +1,40 @@
+import { Directive, Input, OnInit } from '@angular/core';
+import { AbstractControl, ControlContainer, FormGroup } from '@angular/forms';
+
+@Directive({
+  selector: '[fmValidationFeedback]',
+  host: {
+    'class': 'invalid-feedback',
+    '[class.d-none]': 'isValid'
+  }
+})
+export class ValidationFeedbackDirective implements OnInit {
+
+  @Input('fmValidationFeedback') controlName: string;
+  @Input() error: string;
+
+  isValid: boolean;
+
+  private control: AbstractControl;
+
+  constructor(private container: ControlContainer) {}
+
+  ngOnInit() {
+    this.initControlValue();
+    this.listenForErrors();
+  }
+
+  private initControlValue(): void {
+    const form: FormGroup = this.container.formDirective['form'];
+
+    if ( (this.control = form.get(this.controlName)) == null ) {
+      throw new Error(`Couldn't fetch control ${this.controlName}.`);
+    }
+  }
+
+  private listenForErrors(): void {
+    this.control.statusChanges.subscribe(
+      () => this.isValid = !this.control.getError(this.error)
+    );
+  }
+}
