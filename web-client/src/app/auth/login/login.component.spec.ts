c3fec7646cba94298015c9d9444ee53eef7fcb8e
Maciej Marczak <maciej.x.marczak@metrosoft.com>
2018-01-24 15:25:41 +0100

    Implement first test for LoginComponent

--- web-client/src/app/auth/login/login.component.spec.ts
+++ web-client/src/app/auth/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -8,8 +8,20 @@ import { HttpClientModule } from '@angular/common/http';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/delay';
+import {Observable} from "rxjs/Observable";
 
 describe('Component: Login', () => {
+
+  const mockData = {
+    user: {
+      id: 1,
+      email: 'user@test.pl'
+    },
+    credentials: {
+      email: 'user@test.pl',
+      password: 'password'
+    }
+  };
 
   let fixture: ComponentFixture<LoginComponent>;
   let comp: LoginComponent;
@@ -29,6 +41,26 @@ describe('Component: Login', () => {
     fixture = TestBed.createComponent(LoginComponent);
     comp = fixture.componentInstance;
     authService = fixture.debugElement.injector.get(AuthService);
+  });
+
+  it('should navigate to "/" on successful login', () => {
+    const navigateSpy = spyOn(comp['router'], 'navigate');
+    const loginSpy = spyOn(authService, 'login');
+
+    fixture.detectChanges();
+
+    comp.myForm.setValue(mockData.credentials);
+
+    loginSpy.and.callFake(loginForm => {
+      expect(loginForm.email).toBe(mockData.credentials.email);
+      expect(loginForm.password).toBe(mockData.credentials.password);
+      expect(comp.loading).toBeTruthy();
+
+      return Observable.of(mockData.user);
+    });
+
+    comp.login();
+    expect(navigateSpy).toHaveBeenCalledWith(['/']);
   });
 
 });
